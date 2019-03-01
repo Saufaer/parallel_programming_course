0275c89a65145a1d05a5b073d9bdc97867a1bbc6
Oleg <ogretoleg@yandex.ru>
2019-03-01 22:15:53 +0300

    incomprehensible conflict

--- modules/task_1/zinoviev_v_shtrassen_mul_omp/main.cpp
+++ modules/task_1/zinoviev_v_shtrassen_mul_omp/main.cpp
@@ -1,233 +1,204 @@
-// Copyright 2019 Zinoviev Vladimir
+﻿// Copyright 2019 Lalykin Oleg
 
 #include <omp.h>
 #include <iostream>
-#include <random>
+#include <string>
 
-double* matrix_sum(double* A, double* B, int size) {
-  double * C = new double[size];
-  for (int i = 0; i < size; ++i) {
-    C[i] = A[i] + B[i];
-  }
-  return C;
-}
+class Task {
+ public:
+    union Int32 {
+        int x;
+        unsigned char bytes[4];
+    };
+    union Int32 *list;
+    union Int32 *listS;
+    int *Step;
+    int Size;
+    int threads;
 
-double* matrix_sub(double* A, double* B, int size) {
-  double * C = new double[size];
-  for (int i = 0; i < size; ++i) {
-    C[i] = A[i] - B[i];
-  }
-  return C;
-}
+    void initialization(char** argv) {
+        int i;
+        Size = atoi(argv[1]);
 
-double* matrix_mul(double* A, double* B, int n) {
-  int size = n*n;
-  double * C = new double[size];
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      C[i*n + j] = 0;
-      for (int k = 0; k < n; ++k)
-        C[i*n + j] += A[i*n + k] * B[k*n + j];
-    }
-  }
-  return C;
-}
+        list = new union Int32[Size];
+        listS = new union Int32[Size];
 
-double* shtrassen_mul(double* A, double* B, int n) {
-  if (n > 64) {
-    int size = n*n, n_2 = n / 2, size_4 = size / 4;
-    double ** A_ = new double*[4];
-    double ** B_ = new double*[4];
-
-    for (int k = 0; k < 4; ++k) {
-      A_[k] = new double[size_4];
-      B_[k] = new double[size_4];
-    }
-
-    for (int i = 0; i < n_2; ++i) {
-      for (int j = 0; j < n_2; ++j) {
-        A_[0][i*n_2 + j] = A[i*n + j];                      // A_11
-        A_[1][i*n_2 + j] = A[i*n + j + n / 2];              // A_12
-        A_[2][i*n_2 + j] = A[(i + n / 2) * n + j];          // A_21
-        A_[3][i*n_2 + j] = A[(i + n / 2) * n + j + n / 2];  // A_22
-        B_[0][i*n_2 + j] = B[i*n + j];                      // B_11
-        B_[1][i*n_2 + j] = B[i*n + j + n / 2];              // B_12
-        B_[2][i*n_2 + j] = B[(i + n / 2) * n + j];          // B_21
-        B_[3][i*n_2 + j] = B[(i + n / 2) * n + j + n / 2];  // B_22
-      }
-    }
-
-    double ** P = new double*[7];
-    for (int k = 0; k < 7; ++k) {
-      P[k] = new double[size_4];
-    }
-
-    P[0] = shtrassen_mul(matrix_sum(A_[0], A_[3], size_4),
-      matrix_sum(B_[0], B_[3], size_4), n_2);
-    P[1] = shtrassen_mul(matrix_sum(A_[2], A_[3], size_4),
-      B_[0], n_2);
-    P[2] = shtrassen_mul(A_[0],
-      matrix_sub(B_[1], B_[3], size_4), n_2);
-    P[3] = shtrassen_mul(A_[3],
-      matrix_sub(B_[2], B_[0], size_4), n_2);
-    P[4] = shtrassen_mul(matrix_sum(A_[0], A_[1], size_4),
-      B_[3], n_2);
-    P[5] = shtrassen_mul(matrix_sub(A_[2], A_[0], size_4),
-      matrix_sum(B_[0], B_[1], size_4), n_2);
-    P[6] = shtrassen_mul(matrix_sub(A_[1], A_[3], size_4),
-      matrix_sum(B_[2], B_[3], size_4), n_2);
-    double** C = new double*[4];
-    for (int k = 0; k < 4; ++k) {
-      C[k] = new double[size_4];
-    }
-
-    for (int i = 0; i < size_4; ++i) {
-      C[0][i] = P[0][i] + P[3][i] - P[4][i] + P[6][i];
-      C[1][i] = P[2][i] + P[4][i];
-      C[2][i] = P[1][i] + P[3][i];
-      C[3][i] = P[0][i] - P[1][i] + P[2][i] + P[5][i];
-    }
-
-    double* res = new double[size];
-
-    for (int i = 0; i < n_2; ++i)
-      for (int j = 0; j < n_2; ++j) {
-        res[i*n + j] = C[0][i*n_2 + j];                      // C_11
-        res[i*n + j + n / 2] = C[1][i*n_2 + j];              // C_12
-        res[(i + n / 2) * n + j] = C[2][i*n_2 + j];          // C_21
-        res[(i + n / 2) * n + j + n / 2] = C[3][i*n_2 + j];  // C_22
-      }
-    return res;
-  } else {
-    return matrix_mul(A, B, n);
-  }
-}
-
-double* expand_shtrassen(double* A, double* B, int n) {
-  if (n > 450) {
-    int check_pow[8] = { 128, 256, 512, 1024, 2048, 4096, 8192, 16384 };
-    int new_size = 0;
-    for (int i = 0; i < n; ++i) {
-      if (check_pow[i] == n) {
-        return shtrassen_mul(A, B, n);
-      }
-      if (n < check_pow[i]) {
-        new_size = check_pow[i];
-        break;
-      }
-    }
-
-    double* new_A = new double[new_size*new_size];
-    double* new_B = new double[new_size*new_size];
-
-    for (int i = 0; i < new_size; ++i) {
-      for (int j = 0; j < new_size; ++j) {
-        if (i < n && j < n) {
-          new_A[i*new_size + j] = A[i*n + j];
-          new_B[i*new_size + j] = B[i*n + j];
-        } else {
-          new_A[i*new_size + j] = 0.0;
-          new_B[i*new_size + j] = 0.0;
+        for (i = 0; i < Size; i++) {
+            listS[i].x = list[i].x = std::rand() % 50 - 10;
         }
-      }
+
+        threads = atoi(argv[2]);
+        Step = new int[threads + 1];
+
+        for (i = 0; i < threads; i++) {
+            Step[i] = i * Size / threads + (i * Size / threads) % 2;
+        }
+        Step[threads] = Size;
     }
-    double* res_s = nullptr;
-    res_s = shtrassen_mul(new_A, new_B, new_size);
 
-    double* aligned_mat = new double[n*n];
-    for (int i = 0; i < n; ++i)
-      for (int j = 0; j < n; ++j)
-        aligned_mat[i*n + j] = res_s[i*new_size + j];
-
-    return aligned_mat;
-  } else {
-    return matrix_mul(A, B, n);
-  }
-}
-
-void matrix_out(double* A, int n) {
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j)
-      std::cout << A[i*n + j] << " ";
-    std::cout << std::endl;
-  }
-}
-
-bool check(double* A, double* B, int size, int* num, double eps) {
-  for (int i = 0; i < size; ++i)
-    if (fabs(A[i] - B[i]) > eps) {
-      *num = i;
-      return false;
+    void Destructor() {
+        delete[] Step;
+        delete[] list;
+        delete[] listS;
     }
-  return true;
-}
 
-template < typename T >
-T random(T lower_bound, T upper_bound) {
-  return T();
-}
+    void  SortSubsequence(int sign, union Int32* arr, union Int32* tmp, int left, int right) {
+        for (int i = sign; i < left; i += 2) {
+            tmp[i] = arr[i];
+        }
+        union Int32* rightArr = arr + left;
 
-template <>
-int random<int>(int lower_bound, int upper_bound) {
-  std::random_device rd;
-  std::mt19937 rng(rd());
-  std::uniform_int_distribution<int> uni(lower_bound, upper_bound);
-  return uni(rng);
-}
+        int a = sign;
+        int b = sign;
+        int i = sign;
 
-template <>
-double random<double>(double lower_bound, double upper_bound) {
-  std::random_device rd;
-  std::mt19937 rng(rd());
-  std::uniform_real_distribution<double> uni(lower_bound, upper_bound);
-  return uni(rng);
-}
+        while ((a < left) && (b < right)) {
+            if (tmp[a].x <= rightArr[b].x) {
+                arr[i] = tmp[a];
+                a += 2;
+            } else {
+                arr[i] = rightArr[b];
+            }
+            b += 2;
+            i += 2;
+        }
+        if (a == left) {
+            for (int j = b; j < right; j += 2, i += 2) {
+                arr[i] = rightArr[j];
+            }
+        } else {
+            for (int j = a; j < left; j += 2, i += 2) {
+                arr[i] = tmp[j];
+            }
+        }
+    }
 
-int main(int argc, char** argv) {
-  int n;
-  if (argc > 1) {
-    n = atoi(argv[1]);
-  } else {
-    n = random<int>(500, 1000);
-  }
-  std::cout << "n = " << n << std::endl;
-  int size = n*n;
-  std::cout << "size = " << n << "x" << n << " = " << size << std::endl << std::endl;
-  double* A = new double[size], *B = new double[size];
-  double eps = 1e-3;
-  std::cout << "matrix generating..."<< std::endl;
-  for (int i = 0; i < size; ++i) {
-    A[i] = random<double>(0, 10000);
-    B[i] = random<double>(0, 10000);
-  }
-  std::cout << "matrix is generated" << std::endl << std::endl;
+    void PairComparison(union Int32* arr, int size) {
+        for (int i = 1; i < (size + 1) / 2; i++) {
+            if (arr[2 * i].x < arr[2 * i - 1].x) {
+                int tmp = arr[2 * i - 1].x;
+                arr[2 * i - 1] = arr[2 * i];
+                arr[2 * i].x = tmp;
+            }
+        }
+    }
 
-  double* res_mul = nullptr;
-  double* res_shtrass_seq = nullptr;
+    void merge(union Int32* list, int size, int * step, int threads) {
+        union Int32* tmp = new union Int32[size + 1];
+        int i;
+        int level = threads;
+        int *restep = new int[threads + 1];
+        for (int i = 0; i < threads; i++) {
+            step[i] = i * size / threads + (i * size / threads) % 2;
+        }
+        step[threads] = size;
+        while ((level != 1)) {
+            for (i = 0; i < level; i++) {
+                int t = i % 2;
+                SortSubsequence(t, list + step[i - t], tmp + step[i],
+                    step[i + (1 - t)] - step[i - t], step[i + 1 + (1 - t)] - step[i + (1 - t)]);
+            }
+            for (i = 0; i < level; i++) {
+                if (i % 2 != 0) {
+                    PairComparison(list + step[i - 1], step[i + 1] - step[i - 1]);
+                }
+            }
+            level = level / 2;
+            restep = new int[level];
+            restep[0] = step[0];
+            for (int i = 1; i < level; i++) {
+                restep[i] = step[i * 2];
+            }
+            step = new int[level];
+            for (int i = 0; i < level; i++) {
+                step[i] = restep[i];
+            }
+            step[level] = size;
+        }
+    }
 
-  std::cout << "usual multiplicating..." << std::endl;
-  auto s = omp_get_wtime();
-  res_mul = matrix_mul(A, B, n);
-  auto mul_t = omp_get_wtime() - s;
-  std::cout << "done!" << std::endl << std::endl;
-  std::cout << "shtrassen multiplicating..." << std::endl;
-  s = omp_get_wtime();
-  res_shtrass_seq = expand_shtrassen(A, B, n);
-  auto shtrass_seq_t = omp_get_wtime() - s;
-  std::cout << "done!" << std::endl << std::endl;
-  if (n < 10) {
-    matrix_out(res_mul, n);
-    matrix_out(res_shtrass_seq, n);
-  }
+    void Check(union Int32* arr, int size) {
+        bool f = true;
+        for (int i = 0; i < size - 1; i++) {
+            if (arr[i].x > arr[i + 1].x) {
+                f = false;
+            }
+        }
+        if (f) {
+            std::cout << "\n\n CORRECT \n";
+        } else {
+            std::cout << "\n\n FALSE \n";
+        }
+    }
 
-  int bad;
-  std::cout.precision(17);
-  if (check(res_mul, res_shtrass_seq, size, &bad, eps))
-    std::cout << "good multiplication!" << std::endl << std::endl
-    << "time of usual multiplication: " << mul_t << std::endl
-    << "time of sequent shtrassen:" << shtrass_seq_t << std::endl;
-  else
-    std::cout << "bad multiplication! " << bad << " elem: "
-    << std::fixed << res_mul[bad] << "!="
-    << std::fixed << res_shtrass_seq[bad] << std::endl;
+    void PrintList(std::string str, union Int32* arr, int size) {
+        if (size < 33) {
+            std::cout << "\n\n" << str << "\n";
+            for (int i = 0; i < size; i++) {
+                std::cout << arr[i].x << "|";
+            }
+        }
+    }
+
+    void PrintSteps(int * step, int threads) {
+        std::cout << "\n\n" << " Steps:" << "\n";
+        for (int j = 0; j < threads + 1; j++) {
+            std::cout << "=>" << step[j];
+        }
+    }
+
+    void radix_sort_byte(union Int32* arr, int left, int right, int numByte, const int base) {
+        union Int32 *tmp = new union Int32[right - left];
+        int* count = new int[base];
+        for (int i = 0; i < base + 2; i++) {
+            count[i] = 0;
+        }
+        for (int i = left; i < right; i++) {
+            count[arr[i].bytes[numByte] + 1]++;
+        }
+        for (int i = 1; i < base; i++) {
+            count[i] += count[i - 1];
+        }
+        for (int i = left; i < right; i++) {
+            tmp[count[arr[i].bytes[numByte]]++] = arr[i];
+        }
+        for (int i = left; i < right; i++) {
+            arr[i] = tmp[i - left];
+        }
+    }
+
+    void radix_sort(union Int32* arr, int left, int right) {
+        int i;
+        unsigned *subArr = reinterpret_cast<unsigned*>(arr);
+        for (i = left; i < right; i++) {
+            subArr[i] = subArr[i] ^ INT_MIN;
+        }
+        for (i = 0; i < 4; i++) {
+            radix_sort_byte(arr, left, right, i, 256);
+        }
+        for (i = left; i < right; i++) {
+            subArr[i] = subArr[i] ^ INT_MIN;
+        }
+    }
+
+    void RunTask(char **argv) {
+        initialization(argv);
+        PrintList(" Input: ", list, Size);
+        PrintSteps(Step, threads);
+        for (int i = 0; i < threads; i++) {
+            radix_sort(list, Step[i], Step[i + 1]);
+        }
+        PrintList(" After radix: ", list, Size);
+        if (threads > 1) {
+            merge(list, Size, Step, threads);
+        }
+        PrintList(" After merge: ", list, Size);
+        Check(list, Size);
+        Destructor();
+    }
+};
+int main(int argc, char **argv) {
+    Task t;
+    t.RunTask(argv);
+    return 0;
 }
